c78a9be3eaab05755e049b15eb5de98e95fa8d3e
Orban Szilard <orbanszlrd@gmail.com>
2022-03-05 15:43:20 +0100

    Add photos resource

--- server/app.module.ts
+++ server/app.module.ts
@@ -2,13 +2,15 @@ import { Module } from '@nestjs/common';
 import { AngularUniversalModule } from '@nestjs/ng-universal';
 import { join } from 'path';
 import { AppServerModule } from '../src/main.server';
+import { PhotosModule } from './photos/photos.module';
 
 @Module({
   imports: [
     AngularUniversalModule.forRoot({
       bootstrap: AppServerModule,
       viewsPath: join(process.cwd(), 'dist/test-angular-nest/browser')
-    })
+    }),
+    PhotosModule
   ]
 })
 export class AppModule {}
