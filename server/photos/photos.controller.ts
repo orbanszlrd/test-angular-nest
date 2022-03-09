e4c236dd165a3d9f9f05dd455fdef69a1b018c43
Orban Szilard <orbanszlrd@gmail.com>
2022-03-09 11:20:55 +0100

    Add more routes to the PhotosController

--- server/photos/photos.controller.ts
+++ server/photos/photos.controller.ts
@@ -7,11 +7,41 @@ import {
 } from '@nestjs/common';
 import { PhotosService } from './photos.service';
 
-@Controller('photos')
+@Controller('')
 export class PhotosController {
   constructor(private readonly photosService: PhotosService) {}
 
-  @Get()
+  @Get('photos/list')
+  listMediaItems(@Headers('authorization') bearerToken: string) {
+    if (bearerToken === undefined) {
+      throw new UnauthorizedException();
+    }
+
+    return this.photosService.listMediaItems(bearerToken);
+  }
+
+  @Get('photos/search')
+  searchMediaItems(@Headers('authorization') bearerToken: string) {
+    if (bearerToken === undefined) {
+      throw new UnauthorizedException();
+    }
+
+    return this.photosService.searchMediaItems(bearerToken);
+  }
+
+  @Get('photos/:mediaItemId')
+  getMediaItem(
+    @Headers('authorization') bearerToken: string,
+    @Param('mediaItemId') mediaItemId: string
+  ) {
+    if (bearerToken === undefined) {
+      throw new UnauthorizedException();
+    }
+
+    return this.photosService.getMediaItem(bearerToken, mediaItemId);
+  }
+
+  @Get('albums')
   listAlbums(@Headers('authorization') bearerToken: string) {
     if (bearerToken === undefined) {
       throw new UnauthorizedException();
@@ -20,7 +50,7 @@ export class PhotosController {
     return this.photosService.listAlbums(bearerToken);
   }
 
-  @Get(':albumId')
+  @Get('albums/:albumId')
   getAlbum(
     @Headers('authorization') bearerToken: string,
     @Param('albumId') albumId: string
--- server/photos/photos.service.ts
+++ server/photos/photos.service.ts
@@ -4,16 +4,64 @@ import { lastValueFrom } from 'rxjs';
 
 @Injectable()
 export class PhotosService {
+  baseUrl = 'https://photoslibrary.googleapis.com/v1';
+
   constructor(private httpService: HttpService) {}
+
+  async listMediaItems(bearerToken: string) {
+    const result = await lastValueFrom(
+      this.httpService.get(`${this.baseUrl}/mediaItems`, {
+        headers: {
+          Authorization: bearerToken,
+        },
+      })
+    );
+
+    return result.data;
+  }
+
+  async searchMediaItems(bearerToken: string) {
+    const result = await lastValueFrom(
+      this.httpService.post(
+        `${this.baseUrl}/mediaItems:search`,
+        {
+          filters: {
+            mediaTypeFilter: {
+              mediaTypes: ['PHOTO'],
+            },
+          },
+          pageSize: 50,
+        },
+        {
+          headers: {
+            Authorization: bearerToken,
+          },
+        }
+      )
+    );
+
+    return result.data;
+  }
+
+  async getMediaItem(bearerToken: string, mediaItemId: string) {
+    const result = await lastValueFrom(
+      this.httpService.get(`${this.baseUrl}/mediaItems/${mediaItemId}`, {
+        headers: {
+          Authorization: bearerToken,
+        },
+      })
+    );
+
+    return result.data;
+  }
 
   async listAlbums(bearerToken: string) {
     const result = await lastValueFrom(
-      this.httpService.get('https://photoslibrary.googleapis.com/v1/albums', {
+      this.httpService.get(`${this.baseUrl}/albums`, {
         params: {
           pageSize: 50,
         },
         headers: {
-          'Content-Type': 'application/json',
           Authorization: bearerToken,
         },
       })
@@ -24,15 +72,11 @@ export class PhotosService {
 
   async getAlbum(bearerToken: string, albumId: string) {
     const result = await lastValueFrom(
-      this.httpService.get(
-        `https://photoslibrary.googleapis.com/v1/albums/${albumId}`,
-        {
-          headers: {
-            'Content-Type': 'application/json',
-            Authorization: bearerToken,
-          },
-        }
-      )
+      this.httpService.get(`${this.baseUrl}/albums/${albumId}`, {
+        headers: {
+          Authorization: bearerToken,
+        },
+      })
     );
 
     return result.data;
